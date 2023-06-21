6f60d3a2de69043bb998c62bf90f24131b359dc3
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2023-06-21 20:02:41 +0300

    Drawing chart improvements

--- common/main/src/commonMain/kotlin/com/mobiledepro/main/domain/model/Chart.kt
+++ common/main/src/commonMain/kotlin/com/mobiledepro/main/domain/model/Chart.kt
@@ -4,10 +4,47 @@ data class Chart(
     var candleList: List<Candle>
 ) {
     fun getHigherHighPrice(): Double =
-        if (candleList.isNotEmpty()) candleList.sortedByDescending { it.priceHigh }[0].priceHigh else 0.0
+        if (getLimitedCandleList().isNotEmpty()) getLimitedCandleList().sortedByDescending { it.priceHigh }[0].priceHigh else 0.0
 
     fun getLowerLowPrice(): Double =
-        if (candleList.isNotEmpty()) candleList.sortedBy { it.priceLow }[0].priceLow else 0.0
+        if (getLimitedCandleList().isNotEmpty()) getLimitedCandleList().sortedBy { it.priceLow }[0].priceLow else 0.0
 
-    fun candlesCount(): Int = candleList.size
+    // Limit it by 180 by default
+    fun candlesCount(): Int = getLimitedCandleList().size
+
+    /**
+     * Return EMA for every candle
+     */
+    fun calcEMA(period: Int): List<Double> {
+        if (candleList.size < period) return emptyList() //throw RuntimeException("EMA $period cannot be calculated - candle list size ${candleList.size} < $period")
+        val emaList = ArrayList<Double>()
+
+        //With smoothing 2.0 EMA equals to TradingView EMA 50
+        val smoothingFactor = 2.0 / (period + 1)
+        val closePriceList: List<Double> = candleList.mapTo(ArrayList<Double>()) { it.priceClose }
+        // Initialize EMA with the average of the first 200 elements
+
+        //First EMA value is just SMA
+        var ema: Double = closePriceList.subList(0, period).average()
+        emaList.add(ema)
+
+        for (i in period until candleList.size) {
+            val currentValue = candleList[i].priceClose
+            // ema += (currentValue - ema) * smoothingFactor
+            ema = currentValue * smoothingFactor + ema * (1 - smoothingFactor)
+            emaList.add(ema)
+        }
+
+        return emaList
+    }
+
+    fun getLimitedCandleList(): List<Candle> =
+        if (candleList.size > CHART_LIMIT_CANDLE_COUNT)
+            candleList.takeLast(CHART_LIMIT_CANDLE_COUNT)
+        else
+            candleList
+
+    companion object {
+        const val CHART_LIMIT_CANDLE_COUNT = 180 //show only last 180 candles
+    }
 }
--- feature/chart/src/jvmMain/kotlin/com/mobiledevpro/chart/view/ChartBox.kt
+++ feature/chart/src/jvmMain/kotlin/com/mobiledevpro/chart/view/ChartBox.kt
@@ -1,10 +1,14 @@
 package com.mobiledevpro.chart.view
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
@@ -19,38 +23,90 @@ fun ChartBox(chart: Chart, modifier: Modifier = Modifier) {
     val candleRed: Color = MaterialTheme.colors.candleRed
     val candleGreen: Color = MaterialTheme.colors.candleGreen
 
+    val higherHighPrice = remember { mutableStateOf(0.0) }
+    val lowerLowPrice = remember { mutableStateOf(0.0) }
+    val yPixelStep = remember { mutableStateOf(0.0) }
+    val xCandleWith = remember { mutableStateOf(0f) }
+
     WidgetBox(modifier = modifier) {
 
         Canvas(modifier = Modifier.fillMaxSize()) {
             println("Chart Canvas")
-            val higherHighPrice = chart.getHigherHighPrice()
-            val lowerLowPrice = chart.getLowerLowPrice()
+
+            higherHighPrice.value = chart.getHigherHighPrice()
+            lowerLowPrice.value = chart.getLowerLowPrice()
 
             val xSize = size.width
             val ySize = size.height
 
             //Find price movement for 1 px
-            val yPixelStep: Double = higherHighPrice.minus(lowerLowPrice) / ySize
+            yPixelStep.value = higherHighPrice.value.minus(lowerLowPrice.value) / ySize
+
             //Find candle width
-            val xCandleWith: Float = xSize / chart.candlesCount()
+            xCandleWith.value = xSize / chart.candlesCount()
 
             drawXAxis()
             drawYAxis()
-
-            chart.candleList.forEachIndexed { index, candle ->
-                drawCandle(
-                    width = xCandleWith - 1, //1px for space between candles
-                    offsetX = (xCandleWith * index),
-                    lowY = (higherHighPrice.minus(candle.priceLow) / yPixelStep).toFloat(),
-                    highY = (higherHighPrice.minus(candle.priceHigh) / yPixelStep).toFloat(),
-                    openY = (higherHighPrice.minus(candle.priceOpen) / yPixelStep).toFloat(),
-                    closeY = (higherHighPrice.minus(candle.priceClose) / yPixelStep).toFloat(),
-                    color = if (candle.priceOpen < candle.priceClose) candleGreen else candleRed
-                )
-            }
         }
 
+        //Draw the chart
+        Spacer(modifier = Modifier.fillMaxSize().drawWithCache {
+            onDrawBehind {
+                chart.getLimitedCandleList().forEachIndexed { index, candle ->
+                    drawCandle(
+                        width = xCandleWith.value - 1, //1px for space between candles
+                        offsetX = (xCandleWith.value * index),
+                        lowY = (higherHighPrice.value.minus(candle.priceLow) / yPixelStep.value).toFloat(),
+                        highY = (higherHighPrice.value.minus(candle.priceHigh) / yPixelStep.value).toFloat(),
+                        openY = (higherHighPrice.value.minus(candle.priceOpen) / yPixelStep.value).toFloat(),
+                        closeY = (higherHighPrice.value.minus(candle.priceClose) / yPixelStep.value).toFloat(),
+                        color = if (candle.priceOpen < candle.priceClose) candleGreen else candleRed
+                    )
+                }
+            }
+        })
+
+        /*
+        //Draw EMA
+        Spacer(modifier = Modifier.fillMaxSize().drawWithCache {
+
+            val emaPoints : List<Double> = chart.calcEMA(50)
+
+            val path = Path()
+
+            emaPoints.forEach {price ->
+                val pointY
+
+            }
+            path.moveTo(0f, 0f)
+            path.lineTo(size.width / 2f, size.height / 2f)
+            path.lineTo(size.width, 0f)
+            path.close()
+
+            onDrawBehind {
+
+            }
+        })*/
+
+
+        /*
+                    chart.calcEMA(50)
+                        .also {
+                            drawEma(it)
+                        }
+
+         */
+
+
+        /*
+                    chart.calcEMA(200).forEach {
+                        println("EMA 200: $it ")
+                    }
+
+         */
+
     }
+
 }
 
 fun DrawScope.drawXAxis() {
@@ -96,3 +152,24 @@ fun DrawScope.drawCandle(
         strokeWidth = width
     )
 }
+
+/*
+fun DrawScope.drawEma(points: List<Double>) {
+    Spacer(
+        modifier = Modifier
+            .drawWithCache {
+                val path = Path()
+                path.moveTo(0f, 0f)
+                path.lineTo(size.width / 2f, size.height / 2f)
+                path.lineTo(size.width, 0f)
+                path.close()
+                onDrawBehind {
+                    drawPath(path, Color.Magenta, style = Stroke(width = 10f))
+                }
+            }
+            .fillMaxSize()
+    )
+
+}
+
+ */
